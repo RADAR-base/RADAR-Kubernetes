40d2bee0477c90339f4d3aa677c03706fb27231e
Keyvan <keyvan@thehyve.nl>
2022-11-13 23:46:47 +0100

    Use openssl for random number generation

--- bin/util.sh
+++ bin/util.sh
@@ -59,7 +59,8 @@ copy_template() {
 
 generate_secret() {
   size=${1:-30}
-  base64 < /dev/urandom | cut -c -$size | head -c$size | tr '+/' '_.'
+  openssl rand -base64 $size | tr '+/' '_.'
+  #base64 < /dev/urandom | head -c$size | tr '+/' '_.'
 }
 
 query_password() {
