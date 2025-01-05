288a3bc7ed4da0ed25fe1407dccd3a9aaeffd3ab
pvannierop <pim@thehyve.nl>
2025-01-05 14:40:46 +0100

    WIP python behave tests 4

--- test/features/steps/base.py
+++ test/features/steps/base.py
@@ -1,60 +1,36 @@
 import base64
-import os
+
 import yaml
-from functools import reduce  # forward compatibility for Python 3
+from functools import reduce
 import operator
 import requests
 import json
 
-class TestConfig:
-    protocol = os.environ.get('TEST_PROTOCOL', 'http')
-    host = os.environ.get('TEST_HOST', 'localhost')
-    port = os.environ.get('TEST_PORT', 80)
-    org_name = os.environ.get('TEST_ORGANIZATION', 'MAIN')
-    project_name = os.environ.get('TEST_PROJECT', 'test')
-    subject_external_id = os.environ.get('TEST_SUBJECT', 'test_user')
-    # TODO
-    # dev_mode = os.environ.get('DEV_MODE', False)
-    dev_mode = os.environ.get('DEV_MODE', True)
-
-class Cache:
-    management_portal_token = None
-    armt_source_id = None
-    organization_json = None
-    project_json = None
-    armt_project_source_id = None
-    test_subject_id = None
-    secrets = dict(),
-    armt_meta_token = None,
-    armt_refresh_token = None,
-    armt_access_token = None
-
-
-def wait_for_management_portal():
+def wait_for_management_portal(context):
     time = 10
     while True:
         if time < 0:
             raise Exception('Management portal did not start in time')
-        response = requests.get(format_url('managementportal'))
+        response = requests.get(f'{context.config.userdata["url"]}/managementportal')
         if response.status_code == 200:
             break
         time.sleep(1)
         time -= 1
 
-def get_secret(*path_elements):
-    if Cache.secrets is None or len(Cache.secrets[0].keys()) == 0:
-        secrets_file = 'etc/secrets.yaml'
+def get_secret(*path_elements, context):
+    if context.cache["secrets"] is None or len(context.cache["secrets"].keys()) == 0:
+        secrets_file = context.config.userdata["secrets_file"]
         with open(secrets_file, 'r') as file:
-            Cache.secrets = yaml.safe_load(file)
-    return reduce(operator.getitem, path_elements, Cache.secrets)
+            context.cache["secrets"] = yaml.safe_load(file)
+    return reduce(operator.getitem, path_elements, context.cache["secrets"])
 
-def format_url(path):
-    return f'{TestConfig.protocol}://{TestConfig.host}:{TestConfig.port}/{path}'
+def format_url(path, context):
+    return f'{context.config.userdata["url"]}/{path}'
 
-def get_mp_token():
-    if Cache.management_portal_token is not None:
-        return Cache.management_portal_token
-    mp_admin_password = get_secret('management_portal', 'managementportal', 'common_admin_password')
+def get_mp_token(context):
+    if context.cache["management_portal_token"] is not None:
+        return context.cache["management_portal_token"]
+    mp_admin_password = get_secret('management_portal', 'managementportal', 'common_admin_password', context=context)
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -65,38 +41,39 @@ def get_mp_token():
         'password': mp_admin_password,
         'grant_type': 'password',
     }
-    response = requests.post(url=format_url('managementportal/oauth/token'), headers=headers, data=data)
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/oauth/token', headers=headers, data=data)
     assert response.status_code == 200
     management_portal_json = response.json()
     token = management_portal_json['access_token']
     assert token is not None
-    Cache.management_portal_token = token
+    context.cache["management_portal_token"] = token
     return token
 
-def get_source_types():
+def get_source_types(context):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url('managementportal/api/source-types'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/source-types', headers=headers)
     assert response.status_code == 200
     return response.json()
 
-def check_armt_source_type_exists():
-    source_types = get_source_types()
+def check_armt_source_type_exists(context):
+    source_types = get_source_types(context)
     armt_source_type_json = next((item for item in source_types if item["name"] == "RADAR_aRMT"), None)
-    if TestConfig.dev_mode and armt_source_type_json is not None:
-        Cache.armt_source_id = armt_source_type_json['name']
+    if context.config.userdata["dev_mode"] and armt_source_type_json is not None:
+        context.cache["armt_source_id"] = armt_source_type_json['name']
     elif armt_source_type_json is not None:
         raise Exception('aRMT source type already exists')
 
-def create_armt_source_type():
-    if Cache.armt_source_id is not None:
+def create_armt_source_type(context):
+    if context.cache["armt_source_id"] is not None:
         return
+    armt_source_id = "RADAR_aRMT"
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
     data = {
         "appProvider": "org.radarcns.application.ApplicationServiceProvider",
@@ -105,198 +82,204 @@ def create_armt_source_type():
         "catalogVersion": "1.5.0",
         "sourceTypeScope": "ACTIVE",
         "canRegisterDynamically": True,
-        "name": "RADAR_aRMT"
+        "name": armt_source_id
     }
-    response = requests.post(format_url('managementportal/api/source-types'), headers=headers, data=json.dumps(data))
-    assert response.status_code == 200
-    armt_source_id = response.json()['name']
-    assert armt_source_id is not None
-    Cache.armt_source_id = armt_source_id
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/api/source-types', headers=headers, data=json.dumps(data))
+    # 409 means that the armt source already exists.
+    assert response.status_code == 200 or response.status_code == 409
+    if response.status_code == 200:
+        armt_source_id = response.json()['name']
+        assert armt_source_id is not None
+    context.cache["armt_source_id"] = armt_source_id
 
-def check_organization_exists():
+def check_organization_exists(context):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application /json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url('managementportal/api/organizations'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/organizations', headers=headers)
     assert response.status_code == 200
-    test_organization_json = next((item for item in response.json() if item["name"] == TestConfig.org_name), None)
-    if TestConfig.dev_mode and test_organization_json is not None:
-        Cache.organization_json = test_organization_json['name']
+    test_organization_json = next((item for item in response.json() if item["name"] == context.config.userdata["org_name"]), None)
+    if context.config.userdata["dev_mode"] and test_organization_json is not None:
+        context.cache["organization_json"] = test_organization_json['name']
     elif test_organization_json is not None:
         raise Exception('Test organization already exists')
 
-def create_organization():
-    if Cache.organization_json is not None:
+def create_organization(context):
+    if context.cache["organization_json"] is not None:
         return
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
     data = {
-        "name": TestConfig.org_name,
+        "name": context.config.userdata["org_name"],
         "description": "TEST",
         "location": "TEST"
     }
-    response = requests.post(format_url('managementportal/api/organizations'), headers=headers, data=json.dumps(data))
-    assert response.status_code == 200
-    test_organization_json = response.json()
-    assert test_organization_json is not None
-    Cache.organization_json = test_organization_json
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/api/organizations', headers=headers, data=json.dumps(data))
+    # # 409 means that the armt source already exists.
+    # assert response.status_code == 200 or response.status_code == 409
+    # 409 means that the organization already exists
+    assert response.status_code == 200 or response.status_code == 409
+    if response.status_code == 200:
+        test_organization_json = response.json()
+        assert test_organization_json is not None
+    context.cache["organization_json"] = test_organization_json
 
-def check_project_exists():
+def check_project_exists(context):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url('managementportal/api/projects'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/projects', headers=headers)
     assert response.status_code == 200
-    test_project_json = next((item for item in response.json() if item["projectName"] == TestConfig.project_name), None)
-    if TestConfig.dev_mode and test_project_json is not None:
-        Cache.project_json = test_project_json
+    test_project_json = next((item for item in response.json() if item["projectName"] == context.config.userdata["project_name"]), None)
+    if context.config.userdata["dev_mode"] and test_project_json is not None:
+        context.cache["project_json"] = test_project_json
     elif test_project_json is not None:
         raise Exception('Test project already exists')
 
-def create_project():
-    if Cache.project_json is not None:
+def create_project(context):
+    if context.cache["project_json"] is not None:
         return
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
     data = {
-        "organization": Cache.organization_json,
-        "projectName": TestConfig.project_name,
+        "organization": context.cache["organization_json"],
+        "projectName": context.config.userdata["project_name"],
         "description": "TEST",
         "location": "",
-        "sourceTypes": [Cache.armt_source_id],
+        "sourceTypes": [context.cache["armt_source_id"]],
         "startDate": None,
         "endDate": None
     }
-    response = requests.post(format_url('managementportal/api/projects'), headers=headers, data=json.dumps(data))
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/api/projects', headers=headers, data=json.dumps(data))
     assert response.status_code == 200
     test_project_json = response.json()
-    assert test_project_json['projectName'] == TestConfig.project_name
-    Cache.project_json = test_project_json
+    assert test_project_json['projectName'] == context.config.userdata["project_name"]
+    context.cache["project_json"] = test_project_json
 
-def check_armt_project_source_exists():
+def check_armt_project_source_exists(context):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url(f'managementportal/api/projects/{TestConfig.project_name}/sources'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/projects/{context.config.userdata["project_name"]}/sources', headers=headers)
     assert response.status_code == 200
     armt_project_source_json = next((item for item in response.json() if item["sourceType"]["name"] == "RADAR_aRMT"), None)
-    if TestConfig.dev_mode and armt_project_source_json is not None:
-        Cache.armt_project_source_id = armt_project_source_json['sourceId']
+    if context.config.userdata["dev_mode"] and armt_project_source_json is not None:
+        context.cache["armt_project_source_id"] = armt_project_source_json['sourceId']
     elif armt_project_source_json is not None:
         raise Exception('aRMT project source already exists')
 
-def create_armt_project_source():
-    if Cache.armt_project_source_id is not None:
+def create_armt_project_source(context):
+    if context.cache["armt_project_source_id"] is not None:
         return
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
     data = {
         "sourceName": "aRMT-test-source-TEST",
         "assigned": False,
-        "sourceType": Cache.armt_source_id,
-        "project": Cache.organization_json
+        "sourceType": context.cache["armt_source_id"],
+        "project": context.cache["organization_json"]
     }
-    response = requests.post(format_url('managementportal/api/sources'), headers=headers, data=json.dumps(data))
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/api/sources', headers=headers, data=json.dumps(data))
     assert response.status_code == 200
     armt_project_source_json = response.json()
     assert armt_project_source_json['sourceId'] is not None
-    Cache.armt_project_source_id = armt_project_source_json['sourceId']
+    context.cache["armt_project_source_id"] = armt_project_source_json['sourceId']
 
-def check_subject_exists():
+def check_subject_exists(context):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url(f'managementportal/api/projects/{TestConfig.project_name}/subjects'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/projects/{context.config.userdata["project_name"]}/subjects', headers=headers)
     assert response.status_code == 200
-    test_subject_json = next((item for item in response.json() if item["externalId"] == TestConfig.subject_external_id), None)
-    if TestConfig.dev_mode and test_subject_json is not None:
-        Cache.test_subject_id = test_subject_json['login']
+    test_subject_json = next((item for item in response.json() if item["externalId"] == context.config.userdata["subject_external_id"]), None)
+    if context.config.userdata["dev_mode"] and test_subject_json is not None:
+        context.cache["test_subject_id"] = test_subject_json['login']
     elif test_subject_json is not None:
         raise Exception('Test subject already exists')
 
-def create_subject():
-    if Cache.test_subject_id is not None:
+def create_subject(context):
+    if context.cache["test_subject_id"] is not None:
         return
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
     data = {
-        "project": Cache.organization_json,
-        "sources": [Cache.armt_project_source_id],
+        "project": context.cache["organization_json"],
+        "sources": [context.cache["armt_project_source_id"]],
         "status": 1,
-        "externalId": TestConfig.subject_external_id,
+        "externalId": context.config.userdata["subject_external_id"],
         "group": None
     }
-    response = requests.post(format_url('managementportal/api/subjects'), headers=headers, data=json.dumps(data))
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/api/subjects', headers=headers, data=json.dumps(data))
     assert response.status_code == 200
     test_subject_json = response.json()
     assert test_subject_json['login'] is not None
-    Cache.test_subject_id = test_subject_json['login']
+    context.cache["test_subject_id"] = test_subject_json['login']
 
-def get_armt_meta_token():
-    if Cache.armt_meta_token is not None:
-        return Cache.armt_meta_token
+def get_armt_meta_token(context):
+    if context.cache["armt_meta_token"] is not None:
+        return context.cache["armt_meta_token"]
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    data = {
+    params = {
         'clientId': 'aRMT',
-        'login': Cache.test_subject_id,
+        'login': context.cache["test_subject_id"],
         'persistent': True
     }
-    response = requests.post(format_url('managementportal/api/oauth-clients/pair'), headers=headers, data=json.dumps(data))
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/oauth-clients/pair', headers=headers, params=params)
     assert response.status_code == 200
     meta_token = response.json()['tokenName']
     assert meta_token is not None
-    Cache.armt_meta_token = meta_token
+    context.cache["armt_meta_token"] = meta_token
     return meta_token
 
 
-def get_armt_refresh_token():
-    if Cache.armt_refresh_token is not None:
-        return Cache.armt_refresh_token
+def get_armt_refresh_token(context):
+    if context.cache["armt_refresh_token"] is not None:
+        return context.cache["armt_refresh_token"]
     headers = {
-        'Authorization': f'Bearer {get_mp_token()}'
+        'Authorization': f'Bearer {get_mp_token(context)}'
     }
-    response = requests.get(format_url(f'managementportal/api/meta-token/{get_armt_meta_token()}'), headers=headers)
+    response = requests.get(f'{context.config.userdata["url"]}/managementportal/api/meta-token/{get_armt_meta_token(context)}', headers=headers)
     assert response.status_code == 200
     refresh_token = response.json()['refreshToken']
     assert refresh_token is not None
-    Cache.armt_refresh_token = refresh_token
+    context.cache["armt_refresh_token"] = refresh_token
     return refresh_token
 
-
-def get_armt_access_token():
-    if Cache.armt_access_token is not None:
-        return Cache.armt_access_token
+def get_armt_access_token(context):
+    if context.cache["armt_access_token"] is not None:
+        return context.cache["armt_access_token"]
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': f'Basic {base64.b64encode(b"aRMT:").decode("utf-8")}'
     }
+    token = get_armt_refresh_token(context)
     data = {
         'grant_type': 'refresh_token',
-        'refresh_token': get_armt_refresh_token()
+        'refresh_token': token
     }
-    response = requests.post(format_url('managementportal/oauth/token'), headers=headers, data=data)
+    response = requests.post(f'{context.config.userdata["url"]}/managementportal/oauth/token', headers=headers, data=data)
     assert response.status_code == 200
     access_token = response.json()['access_token']
     assert access_token is not None
-    Cache.armt_access_token = access_token
+    context.cache["armt_access_token"] = access_token
     return access_token
--- test/features/steps/init.py
+++ test/features/steps/init.py
@@ -1,57 +1,62 @@
 from behave import *
-from test.features.steps.base import check_armt_source_type_exists, create_armt_source_type, check_organization_exists, \
+from base import check_armt_source_type_exists, create_armt_source_type, check_organization_exists, \
     create_organization, get_mp_token, check_project_exists, create_project, check_armt_project_source_exists, create_armt_project_source, \
     check_subject_exists, create_subject, wait_for_management_portal
 
 
 @given('management portal is running')
 def step_impl(context):
-    wait_for_management_portal()
+    wait_for_management_portal(context)
 
 @then('the management portal token can be requested')
 def step_impl(context):
-    get_mp_token()
+    get_mp_token(context)
 
 @given('retrieval of management portal token')
 def step_impl(context):
-    get_mp_token()
+    get_mp_token(context)
 
-@given('the aRMT source type does not exist')
-def step_impl(context):
-    check_armt_source_type_exists()
+# @given('the aRMT source type does not exist')
+# def step_impl(context):
+#     check_armt_source_type_exists(context)
 
 @then('the aRMT source type can be created')
 def step_impl(context):
-    create_armt_source_type()
+    check_armt_source_type_exists(context)
+    create_armt_source_type(context)
 
-@given('the test organization does not exist')
-def step_impl(context):
-    check_organization_exists()
+# @given('the test organization does not exist')
+# def step_impl(context):
+#     check_organization_exists(context)
 
 @then('the test organization should be created')
 def step_impl(context):
-    create_organization()
+    check_organization_exists(context)
+    create_organization(context)
 
-@given('the test project does not exist')
-def step_impl(context):
-    check_project_exists()
+# @given('the test project does not exist')
+# def step_impl(context):
+#     check_project_exists(context)
 
 @then('the test project should be created')
 def step_impl(context):
-    create_project()
+    check_project_exists(context)
+    create_project(context)
 
-@given('the aRMT project source does not exist')
-def step_impl(context):
-    check_armt_project_source_exists()
+# @given('the aRMT project source does not exist')
+# def step_impl(context):
+#     check_armt_project_source_exists(context)
 
 @then('the aRMT project source should be created')
 def step_impl(context):
-    create_armt_project_source()
+    check_armt_project_source_exists(context)
+    create_armt_project_source(context)
 
-@given('the test subject does not exist')
-def step_impl(context):
-    check_subject_exists()
+# @given('the test subject does not exist')
+# def step_impl(context):
+#     check_subject_exists(context)
 
 @then('the test subject should be created')
 def step_impl(context):
-    create_subject()
+    check_subject_exists(context)
+    create_subject(context)
